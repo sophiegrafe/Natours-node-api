0ac93d3abff5a85a66218dcdb4e13d3ba7a7eaa4
sophiegrafe <sophiegrafe.dev@gmail.com>
2022-02-15 22:46:20 +0100

    fixed the default limit from 1 to 10

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -60,7 +60,7 @@ class APIFeatures {
 
   paginate() {
     const page = +this.queryString.page || 1;
-    const limit = +this.queryString.limit || 1;
+    const limit = +this.queryString.limit || 10;
     const skip = (page - 1) * limit;
 
     this.query = this.query
