fbfcd2c89a22e3229883c4be4171adf7ab67aadf
sophiegrafe <sophiegrafe.dev@gmail.com>
2022-02-15 22:47:25 +0100

    add doc, query & aggregation pre/post middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     ratingsAverage: {
       type: Number,
       default: 4.5,
@@ -71,6 +73,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -83,6 +89,36 @@ tourSchema
   .get(function () {
     return this.duration / 7;
   });
+
+// Document middleware
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Query middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(
+    `Query took ${
+      Date.now() - this.start
+    } milliseconds! `
+  );
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
