2e3612994d60a9d32effe217ad096afaea4ec0d7
sophiegrafe <sophiegrafe.dev@gmail.com>
2022-03-18 21:15:12 +0100

    implemented getAllUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,30 @@
-module.exports = {
-  getAllUsers(req, res) {
-    res.status(500).json({
-      status: 'error',
-      message:
-        'this route is not implemented yet',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(
+  async (req, res, next) => {
+    const users = await User.find();
+
+    // Send the response
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+        users,
+      },
     });
-  },
-  getOneTour(req, res) {
-    res.status(500).json({
-      status: 'error',
-      message:
-        'this route is not implemented yet',
-    });
-  },
-  createUser(req, res) {
-    res.status(500).json({
-      status: 'error',
-      message:
-        'this route is not implemented yet',
-    });
-  },
-  updateUser(req, res) {
-    res.status(500).json({
-      status: 'error',
-      message:
-        'this route is not implemented yet',
-    });
-  },
-  deleteUser(req, res) {
-    res.status(500).json({
-      status: 'error',
-      message:
-        'this route is not implemented yet',
-    });
-  },
-};
+  }
+);
+
+exports.getOneTour = catchAsync(
+  async (req, res, next) => {}
+);
+exports.createUser = catchAsync(
+  async (req, res, next) => {}
+);
+exports.updateUser = catchAsync(
+  async (req, res, next) => {}
+);
+exports.deleteUser = catchAsync(
+  async (req, res, next) => {}
+);
