bd80de0fdac5471ac577df47854341aef0c5c3a2
sophiegrafe <sophiegrafe.dev@gmail.com>
2022-03-18 20:49:21 +0100

    add middleware to protect getAllTours route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -22,7 +23,10 @@ router
   .get(tourController.getMonthlyPlan);
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(
+    authController.protect,
+    tourController.getAllTours
+  )
   .post(tourController.createTour);
 
 router
